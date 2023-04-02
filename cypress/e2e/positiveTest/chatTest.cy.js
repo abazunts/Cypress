362e28cedd87d91339a37ff7127187ce3d5882fc
Nastya Myshko <nmyshko96@mail.ru>
2023-04-02 21:47:03 +0300

    add helpers and chat test

--- cypress/e2e/positiveTest/chatTest.cy.js
+++ cypress/e2e/positiveTest/chatTest.cy.js
@@ -1,16 +1,27 @@
 const loginHelper = require("../../helpers/loginHelper.cy");
-describe('Login test', () => {
+it('Enter "Hello, its an automated test" ', () => {
+    loginHelper.login();
 
-    it('Enter "Hello, its an automated test" ', () => {
-        loginHelper.login();
+    cy.get('.Chat_iconBox__3d1NM')
+        .should('be.visible').click();
 
-        cy.get('.Chat_iconBox__3d1NM')
-            .should('be.visible').click();
+    cy.get('[rows="1"]').type('Hello, its an automated test')
+        .should('be.visible')
 
-        cy.get('[rows="1"]').type('Hello, its an automated test')
-            .should('be.visible')
+    cy.get('.Chat_textInput__2Sr4M > button > img')
+        .should('be.visible').click();
+})
+it('Reply to this message', () => {
+    loginHelper.login();
 
-        cy.get('.Chat_textInput__2Sr4M > button > img')
-            .should('be.visible').click();
-    })
+    cy.get('.Chat_iconBox__3d1NM')
+        .should('be.visible').click();
+
+    cy.get(':nth-child(29) > .Chat_message__BInaT > .Chat_infoWrapper__2pQ7T > button').click();
+
+    cy.get('[rows="1"]').type('Automatic reply to your message :)')
+        .should('be.visible');
+
+    cy.get('.Chat_textInput__2Sr4M > button > img')
+        .should('be.visible').click();
 })
