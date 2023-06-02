037def54100b5950263f45168d8ed3937a733813
Nastya Myshko <nmyshko96@mail.ru>
2023-06-02 12:53:38 +0300

    add changes

--- cypress/e2e/pesochnica/positiveTest/e2e.cy.js
+++ cypress/e2e/pesochnica/positiveTest/e2e.cy.js
@@ -1,167 +1,134 @@
 const loginHelper = require('../../../helpers/posochnica/loginHelper.cy');
 const checkboxHelper = require('../../../helpers/posochnica/checkboxHelper.cy');
+const freeCodeCamp = 'qavaltest4';
+const codewars = 'qavaltest4';
+const adminSite = 'https://sandbox.staging.it-incubator.ru/admin';
+const name = 'Иван';
+const sureName = 'Иванов';
+const fatherName = 'Иванович';
+const dateOfBirth = '11112001';
+const country = 'Беларусь';
+const city = 'Минск';
 
 describe('E2E Testing', () => {
 
     it('E2E test', () => {
+
+        // log in
         loginHelper.login(Cypress.env('login_name'), Cypress.env('login_password'));
 
-        // cy.clock(10000);
-
+        // checkbox
         checkboxHelper.checkBox();
-
-        // ввода никнеймов, привязка телеги и выбор курса
         cy.contains("Обучение в It-Incubator").should('be.visible');
 
+        // admin site
+        cy.visit(adminSite)
 
-        // переход на сайт admin
-        cy.visit('https://sandbox.staging.it-incubator.ru/admin')
-
-
-        // клик на привязку телеги
+        // confirm telegram
         cy.get('.MuiContainer-root > div > :nth-child(1)').should('be.visible').click();
 
         Cypress.on('uncaught:exception', (err, runnable) => {
             return false
         })
 
-        // переход обратно после привязки телеги
-        cy.visit('https://sandbox.staging.it-incubator.ru/');
+        // input freeCodeCamp
+        cy.visit('/');
 
         cy.get(':nth-child(2) > .social-networks-form-page_inputBlockDesktop__2zOYN > .MuiFormControl-root > .MuiInputBase-root > .MuiInputBase-input')
-            .type('qavaltest4')
+            .type(freeCodeCamp)
             .should('be.visible');
 
+        // input codewars
         cy.get(':nth-child(3) > .social-networks-form-page_inputBlockDesktop__2zOYN > .MuiFormControl-root > .MuiInputBase-root > .MuiInputBase-input')
-            .type('qavaltest4')
+            .type(codewars)
             .should('be.visible');
 
-        // кнопка проверить
+        // click check button
         cy.get('button[type="submit"]').should('be.visible').click();
 
-        cy.clock(5000);
-
-
-        // кнопка выбрать курс
+        // click select course button
         cy.get('.social-networks-form-page_continueButtonDesktop__1t7QE > .button-link_button__18xcn')
             .should('be.enabled').click();
 
-        // проверка кнопка продолжить не доступна до выбора курса
-        // cy.get('.course-choice-page_buttons__2DLqJ > .button-link_red__3CQND')
-        //     .as('cont button')
-        //     .should('be.disabled');
+        // continue button is disabled
+        cy.contains('Продолжить').as('cont button').should('be.disabled');
 
-        // клик кнопки фронтенд
+        // select course
         cy.get(':nth-child(9) > .course-choice-page_third__3V6QE > .button-link_button__18xcn')
             .should('be.visible').click();
 
-        // // скролл вниз
-        // cy.get('.course-choice-page_courseChoicePopup__1XGrT')
-        //     .scrollTo('bottomRight');
-
-        // нажать кнопку продолжить
-        cy.get('.course-choice-page_buttons__3fBsv > .button-link_red__3CQND')
-            .as('cont button')
-            .should('be.visible');
-
+        // click continue button
+        cy.contains('Продолжить').as('cont button').should('be.visible');
         cy.get('@cont button').click();
 
         //проверка перехода на следующую страницу
         cy.get('.applicant_mainTextDesktop__3Jx-8')
             .should('be.visible');
 
-        // переход на сайт admin
-        cy.visit('https://sandbox.staging.it-incubator.ru/admin');
+        // admin site
+        cy.visit(adminSite);
 
-        // начислить 60 баллов
-        cy.get('div > :nth-child(9)').click();
-        cy.get('div > :nth-child(9)').click();
-        cy.get('div > :nth-child(9)').click();
+        // add 60 points to each
+        for (let i = 0; i < 3; i++) {
+            cy.get('div > :nth-child(9)').click();
+        }
+        cy.visit('/');
 
-        // переход обратно после начисления баллов
-        cy.visit('https://sandbox.staging.it-incubator.ru/');
-
-        // проверка суммы баллов JS в codewars
+        // check points in codewars JS
         cy.get(':nth-child(3) > .scores_wrapperSubScores__1bv2b > .course-choice-page_subScoreFieldDesktop__2PsPm > .course-choice-page_subScoresWrapper__1tS29 > .course-choice-page_subWrapperInfo__3BnoV > .course-choice-page_userInfo__MooYo > .course-choice-page_scoreInfo__39kvl > span')
             .contains("Баллы: 60");
 
-        // клик кнопки подать заявку
-        cy.get('.course-choice-page_centerButton__2l5Ja > .info-button_menuWrapper__z4N7n > .button-link_button__18xcn').click();
+        // click make a request button
+        cy.contains('Подать заявку').should('be.visible').click();
 
         // проверка кнопки продолжить
         cy.get('.competition-page_continueButton__2cZhu > .info-button_menuWrapper__z4N7n > .button-link_button__18xcn')
             .should('be.visible').click();
 
-        // чекбоксы на странице "подтверди что ты самурай"
+        // checkbox "подтверди что ты самурай"
         checkboxHelper.checkBox();
-
-        // проверка на переход на страницу
         cy.contains('Запросить оплату').should('be.visible');
 
-        // имя
-        cy.get('form > :nth-child(1) > .MuiInputBase-root > .MuiInputBase-input').type("Фы");
+        // name
+        cy.get('form > :nth-child(1) > .MuiInputBase-root > .MuiInputBase-input').type(name);
 
-        // кнопка запросить недоступна
+        // request button is disabled
         cy.get('.personal-info-form-page_buttons__30Z1k > .button-link_red__3CQND')
-            .should('not.be.enabled');
+            .as('request').should('not.be.enabled');
 
-        // фамилия
-        cy.get(':nth-child(2) > .MuiInputBase-root > .MuiInputBase-input').type("Фа");
+        // surname
+        cy.get(':nth-child(2) > .MuiInputBase-root > .MuiInputBase-input').type(sureName);
+        cy.get('@request').should('not.be.enabled');
 
-        // кнопка запросить недоступна
-        cy.get('.personal-info-form-page_buttons__30Z1k > .button-link_red__3CQND')
-            .should('not.be.enabled');
+        // Father's name
+        cy.get(':nth-child(3) > .MuiInputBase-root > .MuiInputBase-input').type(fatherName);
+        cy.get('@request').should('not.be.enabled');
 
-        // отчество
-        cy.get(':nth-child(3) > .MuiInputBase-root > .MuiInputBase-input').type("Фу");
+        // date of birth
+        cy.get(':nth-child(4) > .MuiInputBase-root > .MuiInputBase-input').type(dateOfBirth);
+        cy.get('@request').should('not.be.enabled');
 
-        // кнопка запросить недоступна
-        cy.get('.personal-info-form-page_buttons__30Z1k > .button-link_red__3CQND')
-            .should('not.be.enabled');
-
-        // дата рождения
-        cy.get(':nth-child(4) > .MuiInputBase-root > .MuiInputBase-input').type("11112001");
-
-        // кнопка запросить недоступна
-        cy.get('.personal-info-form-page_buttons__30Z1k > .button-link_red__3CQND')
-            .should('not.be.enabled');
-
-        // пол женский
+        // female gender
         cy.get('.personal-info-form-page_genderChoice__We62Y > :nth-child(2)')
-            .as('sex')
-            .click();
+            .as('gender').click();
+        cy.get('@gender').should('be.enabled');
+        cy.get('@request').should('not.be.enabled');
 
-        cy.get('@sex').should('be.enabled');
-
-        // кнопка запросить недоступна
-        cy.get('.personal-info-form-page_buttons__30Z1k > .button-link_red__3CQND')
-            .should('not.be.enabled');
-
-        // страна
+        // country
         cy.get('.MuiAutocomplete-hasClearIcon > .MuiFormControl-root > .MuiInputBase-root > .MuiAutocomplete-endAdornment').click();
+        cy.contains(country).click();
+        cy.get('@request').should('not.be.enabled');
 
-        cy.contains('Беларусь').click();
-
-        // кнопка запросить недоступна
-        cy.get('.personal-info-form-page_buttons__30Z1k > .button-link_red__3CQND')
-            .should('not.be.enabled');
-
-        // город
+        // city
         cy.get(':nth-child(7) > .MuiFormControl-root > .MuiInputBase-root > .MuiAutocomplete-endAdornment').click();
+        cy.contains(city).click();
+        cy.get('@request').should('not.be.enabled');
 
-        cy.contains('Минск').click();
-
-        // кнопка запросить недоступна
-        cy.get('.personal-info-form-page_buttons__30Z1k > .button-link_red__3CQND')
-            .should('not.be.enabled');
-
-        // чекбокс на странице "заполни личные данные"
+        // checkbox "заполни личные данные"
         cy.get('.jss9').should('not.be.checked').check();
 
         // кнопка запросить доступна
-        cy.get('.personal-info-form-page_buttons__30Z1k > .button-link_red__3CQND')
-            .should('be.enabled');
-
+        cy.get('@request').should('be.enabled');
 
     })
 })
--- cypress/e2e/pesochnica/positiveTest/loginSuccesfulTest.cy.js
+++ cypress/e2e/pesochnica/positiveTest/loginSuccesfulTest.cy.js
@@ -2,8 +2,6 @@ const loginHelper = require('../../../helpers/posochnica/loginHelper.cy');
 describe('Login test', () => {
 
     it('Enter login and password', () => {
-
         loginHelper.login(Cypress.env('login_name'), Cypress.env('login_password'));
-
     })
 })
